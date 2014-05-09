7ddfad7af8bc4112ba7820b5cf80b1531f424ede
Michal Zielinski <michal@zielinscy.org.pl>
2014-05-09 16:29:42 +0200

    add support for getting config by id

--- config_manager.py
+++ config_manager.py
@@ -11,12 +11,15 @@ appname = get_application_id()
 session = None
 CACHE_TIME = 20
 
-def read_sheet(name):
+def read_sheet(name=None, key=None):
     global session
     if not session:
         session = gspread.login(secrets['mail'], secrets['pass'])
 
-    sheet = session.open(name).sheet1
+    if name is not None:
+        sheet = session.open(name).sheet1
+    else:
+        sheet = session.open_by_key(key).sheet1
     cell_values = sheet.get_all_values()
     headers = cell_values[0]
     items = cell_values[2:]
@@ -27,12 +30,13 @@ def read_sheet(name):
             return config
     raise ValueError('no config for %s' % appname)
 
-def get_config(name):
-    conf = memcache.get('config')
+def get_config(name=None, key=None):
+    mcache_key = 'config-%s-%s' % (name, key)
+    conf = memcache.get(mcache_key)
     if not conf:
-        conf = read_sheet(name)
+        conf = read_sheet(name=name, key=key)
         conf = json.dumps(conf)
-        memcache.set('config', conf, CACHE_TIME)
+        memcache.set(mcache_key, conf, CACHE_TIME)
 
     return json.loads(conf)
 
